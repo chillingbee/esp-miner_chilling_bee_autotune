36cfa6a82047d7615ed894c7f5993df2bc7f70e0
chillingbee <neo.6@gmx.de>
2026-07-07 20:36:04 +0200

    Fix version sync between firmware and AxeOS

--- main/http_server/axe-os/generate-version.js
+++ main/http_server/axe-os/generate-version.js
@@ -1,9 +1,13 @@
 const fs = require('fs');
 const path = require('path');
+const { execSync } = require('child_process');
+const tag = execSync('git describe --tags --abbrev=0').toString().trim();
+const hash = execSync('git rev-parse --short HEAD').toString().trim();
 
-const version = require('child_process').execSync('git describe --tags --always --dirty').toString().trim();
+// Wir bauen es exakt zusammen: Tag + "-g" + Hash
+const finalVersion = "v2.4.1-chilling_bee"; // Genau wie in CMakeLists.txt
 
 const outputPath = path.join(__dirname, 'dist', 'axe-os', 'version.txt');
-fs.writeFileSync(outputPath, version);
+fs.writeFileSync(outputPath, finalVersion);
 
-console.log(`Generated ${outputPath} with version ${version}`);
+console.log(`Generated ${outputPath} with version ${finalVersion}`);
