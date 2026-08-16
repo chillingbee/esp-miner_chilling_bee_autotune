e831838594e53141996e7094fd62763312109508
chillingbee <neo.6@gmx.de>
2026-08-16 09:46:54 +0200

    edit max uri handlers:50

--- main/http_server/http_server.c
+++ main/http_server/http_server.c
@@ -1969,7 +1969,7 @@ esp_err_t start_rest_server(GlobalState * global_state)
     config.uri_match_fn = httpd_uri_match_wildcard;
     config.stack_size = 8192;
     config.max_open_sockets = 20;
-    config.max_uri_handlers = 25;
+    config.max_uri_handlers = 50;
     config.close_fn = websocket_close_fn;
     config.lru_purge_enable = true;
 
